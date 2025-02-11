264252ebb0560b6ae2bd0444a19e231c11dae234
Mohammed Zaqout <mo.zaqout@gmail.com>
2025-02-11 22:58:33 +0100

    global loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+  return (
+    <div className='loadingPageContainer'>
+      <h1>الرجاء الانتظار قليلا</h1>
+      <p>يجري تحميل الصفحة</p>
+
+      <div className='loader' />
+    </div>
+  );
+}
