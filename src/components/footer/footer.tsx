28654a5aa8a9d8b515f57c0ea9b9a4477c36fde7
Mohammed Zaqout <mo.zaqout@gmail.com>
2025-02-12 13:25:06 +0100

    footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
 import styles from "./styles.module.css";
 
 export default function FooterComponent() {
   return (
     <div className={styles.footer}>
-      <p>Hello FooterComponent</p>
+      <div className={styles.footerLinks}>
+        <a href='/about'>من نحن</a>
+        <a href='/privacy'>سياسة الخصوصية</a>
+        <a href='/contact'>اتصل بنا</a>
+      </div>
+      <div className={styles.footerLogo}>
+        <Image src='/jazara.svg' alt='Aljazara logo' width={37} height={76} />
+        <div className={styles.footerLogoSlug}>
+          <p>موقع الجزرة نيوز</p>
+          <p>. © 2025</p>
+        </div>
+      </div>
     </div>
   );
 }
