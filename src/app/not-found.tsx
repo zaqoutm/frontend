2ec1fcdae4063613bda9eacc987eb052af8d1c31
Mohammed Zaqout <mo.zaqout@gmail.com>
2025-03-06 06:27:46 +0100

    404 updated

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,24 @@
-export default function NotFoundPage() {
+import Link from "next/link";
+import ListMixedArticles from "../components/listMixedArticles/page";
+import { getMixedLatestArticles } from "../data/sharedArticlesLoader";
+import { StrapiResponse } from "../interfaces/StrapiResponse";
+
+export default async function NotFoundPage() {
+  const mixedArticles: StrapiResponse = await getMixedLatestArticles(10);
+
   return (
-    <div>
-      <h1>الصفحة غير موجودة</h1>
+    <div className='_404_page'>
+      <div className='_404_page_info'>
+        <h1>٤٠٤</h1>
+        <p className='_404_page_info_1'>نعتذر لم نتمكن من العثور على الصفحة المطلوبة:</p>
+        <Link href='/' className='_404_page_info_link'>
+          ALJAZARA.COM ارجع الى الصفحة الرئيسية
+        </Link>
+      </div>
+
+      <div className=''>
+        <ListMixedArticles listTitle='أحدث المقالات' articlesList={mixedArticles} />
+      </div>
     </div>
   );
 }
